807c60228c883b4f8e59d5bbe4a2bf83cd95937b
nilesh9586 <nilesh16mcg34@gmail.com>
2021-01-07 10:10:25 +0530

    Updates

---
+++ precache-manifest.b93feda7aeb6150155548d55bf545d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ad762e953e458cf24c",
+    "url": "/CrickScore/static/css/main.f3848a92.chunk.css"
+  },
+  {
+    "revision": "27ad762e953e458cf24c",
+    "url": "/CrickScore/static/js/main.aa73aab7.chunk.js"
+  },
+  {
+    "revision": "7a0e54e489cc88613197",
+    "url": "/CrickScore/static/js/runtime~main.a5589f98.js"
+  },
+  {
+    "revision": "fc032a7df024b443284e",
+    "url": "/CrickScore/static/js/2.7d034dcd.chunk.js"
+  },
+  {
+    "revision": "6281bad7b6e5b199f3c2",
+    "url": "/CrickScore/static/js/3.4d5c2f64.chunk.js"
+  },
+  {
+    "revision": "3e660c1cfbf8076c79d25d6b31a9f637",
+    "url": "/CrickScore/index.html"
+  }
+];
